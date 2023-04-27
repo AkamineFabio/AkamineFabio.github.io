267c4f7ee4206634f5a3ade309cb904cef113ec8
AkamineFabio <pokeevo43@gmail.com>
2023-04-26 23:00:14 -0300

    First Update
    Added new files

---
+++ Portfólio_Main/scripts.js
@@ -0,0 +1,37 @@
+const menuHamb = document.querySelector('.menuHamb__lista');
+const menuHambIcon = document.querySelector('.menuHamb__container');
+const screenWidth = window.screen.width;
+let turnMenuHambOn = false;
+
+menuHambIcon.addEventListener('click', ()=>{
+    turnMenuHambOn = !turnMenuHambOn;
+    console.log(turnMenuHambOn);
+    if(turnMenuHambOn){
+        menuHamb.style.display = 'block';
+        console.log('ON');
+    } else {
+        menuHamb.style.display = 'none';
+        console.log('OFF');
+    }
+});
+
+if(turnMenuHambOn){
+    menuHamb.style.display = 'block';
+    console.log('ON');
+} else {
+    menuHamb.style.display = 'none';
+    console.log('OFF');
+}
+
+if(screenWidth > 900){
+    turnMenuHambOn = false;
+}
+
+window.addEventListener('resize', ()=>{
+    if(screenWidth > 900){
+        turnMenuHambOn = false;
+        menuHamb.style.display = 'none';
+        console.log('OFF');
+    }
+});
+
