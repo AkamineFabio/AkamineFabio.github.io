c0ed7364f70f08f691b936b7d2aa6e3a12ad5a74
AkamineFabio <pokeevo43@gmail.com>
2023-08-31 15:07:56 -0300

    update projects and skills

--- Portfolio_Main/script.js
+++ Portfolio_Main/script.js
@@ -17,8 +17,8 @@ menuButton.addEventListener('click', () => {
 window.addEventListener('scroll', () => {
     console.log(`Screen position Y: ${window.scrollY}`);
     console.log(`Screen width: ${window.innerWidth}`);
-    if (window.innerWidth <= 900) {
-        if (window.scrollY <= 1700 || window.scrollY >= 3750) {
+    if (window.innerWidth <= 500) {
+        if (window.scrollY <= 1980 || window.scrollY >= 4000) {
             for (let elements of skillBars) {
                 elements.style.animation = 'none';
             }
@@ -33,9 +33,72 @@ window.addEventListener('scroll', () => {
                 elements.style.animation = 'progress 2s ease-in-out forwards';
             }
         }
-    }
-    if (window.innerWidth > 900) {
-        if (window.scrollY <= 1600 || window.scrollY >= 3170) {
+    } else if (window.innerWidth > 500 && window.innerWidth <= 800) {
+        if (window.scrollY <= 1550 || window.scrollY >= 3900) {
+            for (let elements of skillBars) {
+                elements.style.animation = 'none';
+            }
+            for (let elements of skillPers) {
+                elements.style.animation = 'none';
+            }
+        } else {
+            for (let elements of skillBars) {
+                elements.style.animation = 'progress 2s ease-in-out forwards';
+            }
+            for (let elements of skillPers) {
+                elements.style.animation = 'progress 2s ease-in-out forwards';
+            }
+        }
+    } else if (window.innerWidth > 800 && window.innerWidth <= 900) {
+        if (window.scrollY <= 1720 || window.scrollY >= 4020) {
+            for (let elements of skillBars) {
+                elements.style.animation = 'none';
+            }
+            for (let elements of skillPers) {
+                elements.style.animation = 'none';
+            }
+        } else {
+            for (let elements of skillBars) {
+                elements.style.animation = 'progress 2s ease-in-out forwards';
+            }
+            for (let elements of skillPers) {
+                elements.style.animation = 'progress 2s ease-in-out forwards';
+            }
+        }
+    } else if (window.innerWidth > 900 && window.innerWidth <= 1200) {
+        if (window.scrollY <= 1660 || window.scrollY >= 3230) {
+            for (let elements of skillBars) {
+                elements.style.animation = 'none';
+            }
+            for (let elements of skillPers) {
+                elements.style.animation = 'none';
+            }
+        } else {
+            for (let elements of skillBars) {
+                elements.style.animation = 'progress 2s ease-in-out forwards';
+            }
+            for (let elements of skillPers) {
+                elements.style.animation = 'progress 2s ease-in-out forwards';
+            }
+        }
+    } else if (window.innerWidth > 1200 && window.innerWidth <= 1400) {
+        if (window.scrollY <= 1615 || window.scrollY >= 3180) {
+            for (let elements of skillBars) {
+                elements.style.animation = 'none';
+            }
+            for (let elements of skillPers) {
+                elements.style.animation = 'none';
+            }
+        } else {
+            for (let elements of skillBars) {
+                elements.style.animation = 'progress 2s ease-in-out forwards';
+            }
+            for (let elements of skillPers) {
+                elements.style.animation = 'progress 2s ease-in-out forwards';
+            }
+        }
+    } else if (window.innerWidth > 1400) {
+        if (window.scrollY <= 1380 || window.scrollY >= 2960) {
             for (let elements of skillBars) {
                 elements.style.animation = 'none';
             }
