89b4644a7274dc06f2c14529338fcff1dc2a82fb
AkamineFabio <pokeevo43@gmail.com>
2023-08-31 13:38:52 -0300

    update skills bar

--- Portfolio_Main/script.js
+++ Portfolio_Main/script.js
@@ -2,6 +2,9 @@ const menuButton = document.querySelector('.header__button');
 const menu = document.querySelector('.menu');
 let isMenuOpen = false;
 
+const skillBars = document.querySelectorAll('.skill__bar');
+const skillPers = document.querySelectorAll('.bar__per');
+
 menuButton.addEventListener('click', () => {
     isMenuOpen = !isMenuOpen;
     if (isMenuOpen) {
@@ -10,3 +13,42 @@ menuButton.addEventListener('click', () => {
         menu.classList.add('hidden');
     }
 });
+
+window.addEventListener('scroll', () => {
+    console.log(`Screen position Y: ${window.scrollY}`);
+    console.log(`Screen width: ${window.innerWidth}`);
+    if (window.innerWidth <= 900) {
+        if (window.scrollY <= 1700 || window.scrollY >= 3750) {
+            for (let elements of skillBars) {
+                elements.style.animation = 'none';
+            }
+            for (let elements of skillPers) {
+                elements.style.animation = 'none';
+            }
+        } else {
+            for (let elements of skillBars) {
+                elements.style.animation = 'progress 2s ease-in-out forwards';
+            }
+            for (let elements of skillPers) {
+                elements.style.animation = 'progress 2s ease-in-out forwards';
+            }
+        }
+    }
+    if (window.innerWidth > 900) {
+        if (window.scrollY <= 1600 || window.scrollY >= 3170) {
+            for (let elements of skillBars) {
+                elements.style.animation = 'none';
+            }
+            for (let elements of skillPers) {
+                elements.style.animation = 'none';
+            }
+        } else {
+            for (let elements of skillBars) {
+                elements.style.animation = 'progress 2s ease-in-out forwards';
+            }
+            for (let elements of skillPers) {
+                elements.style.animation = 'progress 2s ease-in-out forwards';
+            }
+        }
+    }
+})
--- Portfolio_Main/scrollReveal.js
+++ Portfolio_Main/scrollReveal.js
@@ -3,10 +3,6 @@ const apresentacao = document.querySelector('.apresentacao');
 const sobremim = document.querySelector('.sobremim');
 const habilidades = document.querySelector('.habilidades');
 
-
-const skillBars = document.querySelectorAll('.skill__bar');
-const skillPers = document.querySelectorAll('.bar__per');
-
 window.sr = ScrollReveal({ reset: true });
 
 for (let i of scrollContainer) {
@@ -17,13 +13,31 @@ sr.reveal(apresentacao, { duration: 2000 });
 sr.reveal(sobremim, { duration: 2000 });
 sr.reveal(habilidades, { duration: 2000 });
 
+/* window.addEventListener('scroll', () => {
+    if (!habilidades.checkVisibility()) {
+        console.log('invisivel');
+        for (let elements of skillBars) {
+            elements.style.animation = 'none';
+        }
+        for (let elements of skillPers) {
+            elements.style.animation = 'none';
+        }
+        return;
+    }
 
-if (habilidades.checkVisibility()) {
-    for (let elements of skillBars) {
-        elements.classList.add('skill__show-anim');
+    if (habilidades.checkVisibility()) {
+        console.log('visivel');
+        for (let elements of skillBars) {
+            elements.classList.add('skill__show-anim');
+            elements.style.animation = 'progress 2s ease-in-out forwards';
+        }
+        for (let elements of skillPers) {
+            elements.classList.add('skill__show-anim');
+            elements.style.animation = 'progress 2s ease-in-out forwards';
+        }
+        return;
     }
-    for (let elements of skillPers) {
-        elements.classList.add('skill__show-anim');
-    }
-}
+}) */
+
+
 
