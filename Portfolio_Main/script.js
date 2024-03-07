9dbb651fad0a8d8735132b723e0ca5b8686ca611
AkamineFabio <pokeevo43@gmail.com>
2024-03-07 16:15:07 -0300

    'update'

--- Portfolio_Main/script.js
+++ Portfolio_Main/script.js
@@ -2,8 +2,46 @@ const menuButton = document.querySelector('.header__button');
 const menu = document.querySelector('.menu');
 let isMenuOpen = false;
 
-/* const skillBars = document.querySelectorAll('.skill__bar');
-const skillPers = document.querySelectorAll('.bar__per'); */
+const allFlipCards = document.querySelectorAll('.flipcard');
+
+let hasTouchScreen = false;
+let isFlipped = false;
+
+if ("maxTouchPoints" in navigator) {
+    hasTouchScreen = navigator.maxTouchPoints > 0;
+} else if ("msMaxTouchPoints" in navigator) {
+    hasTouchScreen = navigator.msMaxTouchPoints > 0;
+} else {
+    var mQ = window.matchMedia && matchMedia("(pointer:coarse)");
+    if (mQ && mQ.media === "(pointer:coarse)") {
+        hasTouchScreen = !!mQ.matches;
+    } else if ('orientation' in window) {
+        hasTouchScreen = true; // deprecated, but good fallback
+    } else {
+        // Only as a last resort, fall back to user agent sniffing
+        var UA = navigator.userAgent;
+        hasTouchScreen = (
+            /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+            /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
+        );
+    }
+}
+
+if (hasTouchScreen) {
+    for (let flip of allFlipCards) {
+        flip.addEventListener('click', () => {
+            isFlipped = !isFlipped;
+            if (isFlipped) {
+                console.log(isFlipped);
+                flip.children[0].style.transform = 'rotateY(180deg)';
+            } else {
+                flip.children[0].style.transform = 'rotateY(0deg)';
+            }
+        });
+
+    }
+}
+
 
 menuButton.addEventListener('click', () => {
     isMenuOpen = !isMenuOpen;
@@ -13,105 +51,3 @@ menuButton.addEventListener('click', () => {
         menu.classList.add('hidden');
     }
 });
-
-/* window.addEventListener('scroll', () => {
-         console.log(`Screen position Y: ${window.scrollY}`);
-        console.log(`Screen width: ${window.innerWidth}`); 
-    if (window.innerWidth <= 500) {
-        if (window.scrollY <= 1980 || window.scrollY >= 4000) {
-            for (let elements of skillBars) {
-                elements.style.animation = 'none';
-            }
-            for (let elements of skillPers) {
-                elements.style.animation = 'none';
-            }
-        } else {
-            for (let elements of skillBars) {
-                elements.style.animation = 'progress 2s ease-in-out forwards';
-            }
-            for (let elements of skillPers) {
-                elements.style.animation = 'progress 2s ease-in-out forwards';
-            }
-        }
-    } else if (window.innerWidth > 500 && window.innerWidth <= 800) {
-        if (window.scrollY <= 1550 || window.scrollY >= 3900) {
-            for (let elements of skillBars) {
-                elements.style.animation = 'none';
-            }
-            for (let elements of skillPers) {
-                elements.style.animation = 'none';
-            }
-        } else {
-            for (let elements of skillBars) {
-                elements.style.animation = 'progress 2s ease-in-out forwards';
-            }
-            for (let elements of skillPers) {
-                elements.style.animation = 'progress 2s ease-in-out forwards';
-            }
-        }
-    } else if (window.innerWidth > 800 && window.innerWidth <= 900) {
-        if (window.scrollY <= 1720 || window.scrollY >= 4020) {
-            for (let elements of skillBars) {
-                elements.style.animation = 'none';
-            }
-            for (let elements of skillPers) {
-                elements.style.animation = 'none';
-            }
-        } else {
-            for (let elements of skillBars) {
-                elements.style.animation = 'progress 2s ease-in-out forwards';
-            }
-            for (let elements of skillPers) {
-                elements.style.animation = 'progress 2s ease-in-out forwards';
-            }
-        }
-    } else if (window.innerWidth > 900 && window.innerWidth <= 1200) {
-        if (window.scrollY <= 1660 || window.scrollY >= 3230) {
-            for (let elements of skillBars) {
-                elements.style.animation = 'none';
-            }
-            for (let elements of skillPers) {
-                elements.style.animation = 'none';
-            }
-        } else {
-            for (let elements of skillBars) {
-                elements.style.animation = 'progress 2s ease-in-out forwards';
-            }
-            for (let elements of skillPers) {
-                elements.style.animation = 'progress 2s ease-in-out forwards';
-            }
-        }
-    } else if (window.innerWidth > 1200 && window.innerWidth <= 1400) {
-        if (window.scrollY <= 1615 || window.scrollY >= 3180) {
-            for (let elements of skillBars) {
-                elements.style.animation = 'none';
-            }
-            for (let elements of skillPers) {
-                elements.style.animation = 'none';
-            }
-        } else {
-            for (let elements of skillBars) {
-                elements.style.animation = 'progress 2s ease-in-out forwards';
-            }
-            for (let elements of skillPers) {
-                elements.style.animation = 'progress 2s ease-in-out forwards';
-            }
-        }
-    } else if (window.innerWidth > 1400) {
-        if (window.scrollY <= 1380 || window.scrollY >= 2960) {
-            for (let elements of skillBars) {
-                elements.style.animation = 'none';
-            }
-            for (let elements of skillPers) {
-                elements.style.animation = 'none';
-            }
-        } else {
-            for (let elements of skillBars) {
-                elements.style.animation = 'progress 2s ease-in-out forwards';
-            }
-            for (let elements of skillPers) {
-                elements.style.animation = 'progress 2s ease-in-out forwards';
-            }
-        }
-    }
-}) */
