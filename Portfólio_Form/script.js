58faa6ba32720a9d95420293caca8ae1a36fecab
AkamineFabio <pokeevo43@gmail.com>
2024-04-25 12:02:10 -0300

    update form

--- Portfólio_Form/script.js
+++ Portfólio_Form/script.js
@@ -4,30 +4,36 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-form.addEventListener('submit', e =>{
+form.addEventListener('submit', e => {
     e.preventDefault();
 
-    validateInputs();
+    if (validateInputs()) {
+        alert('FORMULÁRIO APROVADO!')
+        console.log('passou');
+    } else {
+        alert('FORMULÁRIO NÃO APROVADO!')
+        console.log('não passou');
+    }
 });
 
 
-username.addEventListener('blur', e =>{
+username.addEventListener('blur', e => {
     usernameValidation();
 });
 
-email.addEventListener('blur', e =>{
+email.addEventListener('blur', e => {
     emailValidation();
 });
 
-password.addEventListener('blur', e =>{
+password.addEventListener('blur', e => {
     passwordValidation();
 });
 
-password2.addEventListener('blur', e =>{
+password2.addEventListener('blur', e => {
     password2Validation();
 });
 
-const setError = (element , message) =>{
+const setError = (element, message) => {
     const inputControl = element.parentElement;
     const errorDisplay = inputControl.querySelector('.error');
 
@@ -37,7 +43,7 @@ const setError = (element , message) =>{
     console.log(inputControl);
 }
 
-const setSuccess = (element)=>{
+const setSuccess = (element) => {
     const inputControl = element.parentElement;
     const errorDisplay = inputControl.querySelector('.error');
 
@@ -46,70 +52,82 @@ const setSuccess = (element)=>{
     inputControl.classList.remove('input-error');
 }
 
-const usernameValidation = ()=>{
+const usernameValidation = () => {
     const usernameValue = username.value.trim();
 
-    if(usernameValue === ''){
-        setError(username,'*Username is required');
-    } 
-    else{
+    if (usernameValue === '') {
+        setError(username, '*Username is required');
+        return false;
+    }
+    else {
         setSuccess(username);
+        return true;
     }
 }
 
-const emailValidation = ()=>{
+const emailValidation = () => {
     const emailValue = email.value.trim();
 
-    if(emailValue === ''){
-        setError(email,'*Email is required');
-    } else if(!isValidEmail(emailValue)){
-        setError(email,'*Provide a valid email address');
+    if (emailValue === '') {
+        setError(email, '*Email is required');
+        return false;
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, '*Provide a valid email address');
+        return false;
     }
-    else{
+    else {
         setSuccess(email);
+        return true;
     }
 }
 
-const isValidEmail = (email)=>{
+const isValidEmail = (email) => {
     const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
     return re.test(String(email).toLowerCase()); //verify in the email that you passed on the parameter if it has some of this symbols 
 }
 
 
-const passwordValidation = ()=>{
+const passwordValidation = () => {
     const passwordValue = password.value.trim();
 
-    if(passwordValue === ''){
-        setError(password,'*Password is required');
-    } else if(passwordValue.length < 8){
-        setError(password , '*Password must be at least 8 character.')
+    if (passwordValue === '') {
+        setError(password, '*Password is required');
+        return false;
+    } else if (passwordValue.length < 8) {
+        setError(password, '*Password must be at least 8 character.');
+        return false;
     }
-    else{
+    else {
         setSuccess(password);
+        return true;
     }
 }
 
-const password2Validation = ()=>{
+const password2Validation = () => {
     const passwordValue = password.value.trim();
     const password2Value = password2.value.trim();
 
-    if(password2Value === ''){
-        setError(password2,'*Password is required');
-    } else if(password2Value.length < 8){
-        setError(password2 , '*Password2 must be at least 8 character.')
-    } else if(password2Value !== passwordValue){
-        setError(password2 , "*Password doesn't match");
+    if (password2Value === '') {
+        setError(password2, '*Password is required');
+        return false;
+    } else if (password2Value.length < 8) {
+        setError(password2, '*Password2 must be at least 8 character.')
+        return false;
+    } else if (password2Value !== passwordValue) {
+        setError(password2, "*Password doesn't match");
+        return false;
     }
-    else{
+    else {
         setSuccess(password2);
+        return true;
     }
 }
 
-const validateInputs = ()=>{
-
-    usernameValidation();
-    emailValidation();
-    passwordValidation();
-    password2Validation();
+const validateInputs = () => {
+    if (usernameValidation() && emailValidation() && passwordValidation() && password2Validation()) {
+        return true;
+    } else {
+        return false;
+    }
 
 }
